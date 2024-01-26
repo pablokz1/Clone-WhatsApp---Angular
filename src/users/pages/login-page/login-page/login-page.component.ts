c88fc579f5e9123fc129b811378e42059782d21e
pablokz1 <pablo.carvalho16@hotmail.com>
2024-01-25 22:56:35 -0300

    [init] - rota de login

---
+++ src/users/pages/login-page/login-page/login-page.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      login-page works!
+    </p>
+  `,
+  styleUrl: './login-page.component.scss'
+})
+export class LoginPageComponent {
+
+}
